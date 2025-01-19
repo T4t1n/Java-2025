81f482ec1e4251ea835b77bfeee9d96eb0b3789b
T4t1n <jaimiskytn@gmail.com>
2025-01-19 14:24:55 -0600

    Commit Java, Finalizacion Mad Libs Clone 19/01/2025

--- Projects/Proj2_MadLibsClone/src/main/java/com/mycompany/proj2_madlibsclone/Proj2_MadLibsClone.java
+++ Projects/Proj2_MadLibsClone/src/main/java/com/mycompany/proj2_madlibsclone/Proj2_MadLibsClone.java
@@ -15,7 +15,7 @@ public class Proj2_MadLibsClone {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String Adjective1, Girls_Name, Adjective2, Occupation1, Place, Clothing,
-                Hobby, Adjective3, Ocupation2, Boys_name, Mans_name;
+                Hobby, Adjective3, Occupation2, Boys_name, Mans_name;
         System.out.println("Enter an adjective: ");
         Adjective1 = input.next();
         System.out.println("Enter a girl's name: ");
@@ -24,6 +24,26 @@ public class Proj2_MadLibsClone {
         Adjective2 = input.next();
         System.out.println("Enter an occupation: ");
         Occupation1 = input.next();
+        System.out.println("Enter the name of a place: ");
+        Place = input.next();
+        System.out.println("Enter the name of a piece of clothing: ");
+        Clothing = input.next();
+        System.out.println("Enter the name of a hobby: ");
+        Hobby = input.next();
+        System.out.println("Enter another adjective: ");
+        Adjective3 = input.next();
+        System.out.println("Enter another occupation: ");
+        Occupation2 = input.next();
+        System.out.println("Enter a boy's name: ");
+        Boys_name = input.next();
+        System.out.println("Enter a man's name: ");
+        Mans_name = input.next();
         
-    }
+        
+        System.out.println("There once was a " + Adjective1 + " girl named " + Girls_Name +
+                            " who was a " + Adjective2 + " " + Occupation1 + " in the kingdom of " + Place + "." + '\n' + 
+                            "She loved to wear " + Clothing + " and to " + Hobby + "." + " She wanted to marry the " + 
+                            Adjective3 + Occupation2 + " named " + Boys_name + " but her father, king " + Mans_name + " " +
+                            " forbid her from seeing him.");
+    }// End method main
 }
