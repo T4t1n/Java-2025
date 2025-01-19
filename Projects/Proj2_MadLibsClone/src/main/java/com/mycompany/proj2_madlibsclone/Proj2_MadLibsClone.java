a1ba2e5ff40fcecc4630497068bd798987b9b013
T4t1n <jaimiskytn@gmail.com>
2025-01-18 23:58:58 -0600

    Mi segundo commit Java, Avance Mad Libs Clone 18/01/2025

---
+++ Projects/Proj2_MadLibsClone/src/main/java/com/mycompany/proj2_madlibsclone/Proj2_MadLibsClone.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proj2_madlibsclone;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cmjyt
+ */
+public class Proj2_MadLibsClone {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Adjective1, Girls_Name, Adjective2, Occupation1, Place, Clothing,
+                Hobby, Adjective3, Ocupation2, Boys_name, Mans_name;
+        System.out.println("Enter an adjective: ");
+        Adjective1 = input.next();
+        System.out.println("Enter a girl's name: ");
+        Girls_Name = input.next();
+        System.out.println("Enter another adjective: ");
+        Adjective2 = input.next();
+        System.out.println("Enter an occupation: ");
+        Occupation1 = input.next();
+        
+    }
+}
