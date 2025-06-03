26f4fc203cca114fb45df96406f09deefa5d2700
T4t1n <jaimiskytn@gmail.com>
2025-06-02 21:48:46 -0600

    test 2/06/25

---
+++ Practice/Clase2/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class Ejercicio1 {
+
+    double resultado = 6 / 2 * (1+2);
+
+    public static void main(String[] args) {
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        System.out.println(ejercicio1.resultado);
+        //El resultado es 9, Probablmente sea el = que colocaron de ultimo en la artimetica
+    }
+}
