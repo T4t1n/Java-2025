ca2dbb1466b5c0bcbe1fb1fe2080275efff3364b
T4t1n <jaimiskytn@gmail.com>
2025-09-01 15:25:34 -0600

    Actualizacion 01/09/2025

---
+++ Learning java/Informaticonfig/POO/src/Clases/Principal.java
@@ -0,0 +1,19 @@
+package Clases;
+
+
+
+class Prestamo{
+    double cuota;
+
+    Prestamo(double cuota) {
+        this.cuota = cuota;
+    }
+
+}
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+}
