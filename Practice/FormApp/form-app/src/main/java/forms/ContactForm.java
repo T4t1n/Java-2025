f93cc6bb7161b583be2a315db11721b1f04ccf83
T4t1n <jaimiskytn@gmail.com>
2025-12-02 12:31:08 -0600

    Upload

--- Practice/FormApp/form-app/src/main/java/forms/ContactForm.java
+++ Practice/FormApp/form-app/src/main/java/forms/ContactForm.java
@@ -1,8 +1,19 @@
 package forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ContactForm {
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Must be a valid email")
+    @NotBlank(message= "Email is required")
     private String email;
+
+    @Size(min = 10, message = "Message must be at least 10 characters long")
     private String message;
 
     public String getName() {
--- Practice/FormApp/form-app/src/main/java/forms/web/ContactController.java
+++ Practice/FormApp/form-app/src/main/java/forms/web/ContactController.java
@@ -1,8 +1,10 @@
 package forms.web;
 
 import forms.ContactForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,17 @@ public class ContactController {
 
     @GetMapping("/contact")
     public String showForm(Model model) {
-        model.addAttribute("ContactForm", new ContactForm());
+        model.addAttribute("contactForm", new ContactForm());
         return "contact";
     }
 
     @PostMapping("/contact")
-    public String submitForm(@ModelAttribute ContactForm contactForm, Model model) {
+    public String submitForm(@Valid @ModelAttribute ContactForm contactForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "contact";
+        }
+
         model.addAttribute("contactForm", contactForm);
         return "result";
     }
