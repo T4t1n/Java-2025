6207ee2a645f821fa867dee47a1ad8a56bf77985
T4t1n <jaimiskytn@gmail.com>
2025-12-16 23:52:55 -0600

    Upload

---
+++ Practice/Tasks/Collections/src/main/java/com/t4t1n/collections/Collections.java
@@ -0,0 +1,26 @@
+
+
+package com.t4t1n.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ *
+ * @author Jaimisky
+ */
+public class Collections {
+
+    public static void main(String[] args) {
+        ArrayList<String> colors;
+        HashSet<String> uniqueColors;
+        HashMap<String, String> useColors;
+        
+        
+        colors.add("Red");
+        
+        
+        
+    }
+}
