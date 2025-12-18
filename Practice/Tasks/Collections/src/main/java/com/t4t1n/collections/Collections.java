882c3b0bee7591e926dee918dddcd2612dc31ccd
T4t1n <jaimiskytn@gmail.com>
2025-12-17 22:02:40 -0600

    Upload

--- Practice/Tasks/Collections/src/main/java/com/t4t1n/collections/Collections.java
+++ Practice/Tasks/Collections/src/main/java/com/t4t1n/collections/Collections.java
@@ -11,14 +11,42 @@ import java.util.HashSet;
  * @author Jaimisky
  */
 public class Collections {
-
+    
     public static void main(String[] args) {
-        ArrayList<String> colors;
-        HashSet<String> uniqueColors;
-        HashMap<String, String> useColors;
+        ArrayList<String> colors = new ArrayList<>();
+        HashSet<String> uniqueColors = new HashSet<>();
+        HashMap<String, String> useColors = new HashMap<>();
         
         
         colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Green");
+        
+        System.out.println(colors);
+        
+        
+        uniqueColors.add("Red");
+        uniqueColors.add("Blue");
+        uniqueColors.add("Green");
+        uniqueColors.add("Green");
+        
+        System.out.println(uniqueColors);
+        
+        useColors.put("Cambiaso", "Red");
+        useColors.put("Locatelli", "Green");
+        useColors.put("Gatti", "Gray");
+        useColors.put("Gatti", "Gray");
+        useColors.put("Kelly", "Gray");
+        
+        System.out.println(useColors);
+        
+        
+        /*  ###### Why duplicates behave differently ##### */
+        
+        
+        
+        
         
         
         
